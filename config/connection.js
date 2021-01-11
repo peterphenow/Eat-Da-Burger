455b677b58fbaeff41611143e86a7b2ec56ccae3
Peter Phenow <peter.phenow@gmail.com>
2021-01-10 22:28:31 -0600

    fix connection syntax error

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ let mysql = require("mysql");
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  let connection = mysql.createConnection({
+  connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
